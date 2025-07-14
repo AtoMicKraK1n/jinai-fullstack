dabb7ffb632f939b8d7558532bded590ce1cd1ba
AtoMicKraK1n <prakharsharma2506@gmail.com>
2025-07-14 21:01:05 +0400

    some fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import "./app.css";
 import { Providers } from "./providers";
+import ParticleBackground from "./components/ParticleBackground";
 
 export const metadata = {
   title: "JinAI",
